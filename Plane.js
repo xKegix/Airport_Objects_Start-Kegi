0ad161c21d5cde4c7ccdf0aeb3a8627606e0486d
Elvis Garcia <elvisgarcia93@gmail.com>
2022-02-22 17:48:26 -0500

    Add Plane Solution to Starter Code

--- Plane.js
+++ Plane.js
@@ -1,15 +1,21 @@
 class Plane {
     constructor(name){
-
+        this.name = name
+        this.origin = ''
+        this.destination = ''
+        this.passengers = []
     }
 
     setOrigin(origin) {
+        this.origin = origin
     }
 
     setDestination(destination) {
+        this.destination = destination
     }
 
     addPassenger(passenger) {
+        this.passengers.push(passenger)
     }
 }
 
